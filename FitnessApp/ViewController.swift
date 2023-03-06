ac71c1e2eb5108df54ecd4b5182e21cad54f16e4
Aleksey Trushkov <flvtrv@icloud.com>
2023-03-06 23:26:04 +0300

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Алексей Трушков on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
