8d243f1876c69657897a54db95f7e37ca8ea1b12
Aleksey Trushkov <flvtrv@icloud.com>
2023-03-09 14:52:43 +0300

    add tabBar

---
+++ FitnessApp/NewWorkoutModule/Controllers/NewWorkoutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NewWorkoutViewController.swift
+//  FitnessApp
+//
+//  Created by Алексей Трушков on 09.03.2023.
+//
+
+import UIKit
+
+class NewWorkoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
